4b5694b649a175e3874d5cf4f882509a6943378f
AyaanZaidi <103330931+AyaanZaidi@users.noreply.github.com>
2022-10-31 01:19:39 +0500

    java practice
    java start

---
+++ app.js
@@ -0,0 +1,62 @@
+alert("Hallo Ayaan!");
+// chap 1: complete
+
+var firstname = "ayaan"
+var lastname = "zaidi"
+// chap 2: complete 
+
+var weight = 150
+var abc = 100
+var total = weight + abc
+alert(total)
+// chap 3: complete 
+
+var first name = "ayaan"
+//  illegal variable
+var $ab3434_ = "ayaan"
+//  legal variable
+
+ var var = "ayaan"
+//  illegal variable
+
+var ayaanZaidi = "name"
+// legal variable
+// chap 4: complete 
+
+
+var firstNumber = 12 + 24;
+var secNumber = 12 - 24;
+var thirdNumber = 12 * 24;
+var fourthNumber = 12 / 24;
+alert(firstNumber)
+alert(secNumber)
+alert(thirdNumber)
+alert(fourthNumber)
+// it is run
+// chap 5: complete
+
+
+var num = 1;
+var newNum = num++;
+
+
+var num = 1;
+var newNum = num--;
+// chap 6: complete
+
+
+var amount = 3 + (1 * 6);
+// use BODMAS rule
+// chap 7: complete
+
+var year = 20 +"22"
+alert(year)
+// this is concadination
+// chap 8: complete
+
+
+
+
+
+
+
